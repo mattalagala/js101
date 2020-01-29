c6212fe14fbc4d0986c8194c12ad1502aa559886
Alicia Alagala <aliciaalagala@Alicias-MacBook-Pro.local>
2020-01-28 21:00:41 -0600

    Completed 102-undefined-null.js

--- exercises/102-undefined-booleans-null.js
+++ exercises/102-undefined-booleans-null.js
@@ -3,13 +3,29 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeNothing"
+
+function makeNothing () {
+var huh;
+return huh
+
+}
 // 2) Declare a variable "huh" and assign nothing to it, then return "huh".
 //    NOTE: the value undefined means "lack of assignment", which is what this
 //          function should return
 
 
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeBoolean"
+
+function makeBoolean () {
+
+var myBool = true;
+return myBool
+
+}
+
 // 2) Declare a variable "myBool" and assign it either true or false, then return "myBool"
 //    NOTE: remember that the string "true" (surrounded by quotes) is not
 //          the same thing as boolean true (no quotes)
@@ -17,14 +33,35 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeTrue"
+
+function makeTrue () {
+
+var yup = true;
+return yup
+
+}
 // 2) Declare a variable "yup" and assign boolean true, then return "yup"
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeFalse"
+
+function makeFalse () {
+
+var nope = false;
+return nope
+
+}
 // 2) Declare a variable "nope" and assign boolean false, then return "nope"
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeNull"
+
+function makeNull () {
+
+var nothingMuch = null;
+return nothingMuch
+
+}
 // 2) Declare a variable "nothingMuch" and assign null to it, then return "nothingMuch"
